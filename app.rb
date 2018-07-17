9c7aa4c864509c61b5f8df73f6f5a9bcb319f9fe
Ella Zhang <eyanchizhang@gmail.com>
2018-07-17 18:33:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   # This is a sample static route.
+  get '/' do
+    "This is the homepage"
+  end
   get '/hello' do
     "Hello World!"
   end
